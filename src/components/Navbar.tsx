98c8cb880838dd8e2e56a9b51547dbaef35a3e7b
JN <napolitanjoe@gmail.com>
2024-12-31 16:59:35 -0700

    fix image path for npm build

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import "../styles/NavBarStyles.css";
+import emblem from "/src/assets/react-emblem.png";
 
 interface Props {
   onClick: () => void;
@@ -8,7 +9,7 @@ const Navbar = ({ onClick }: Props) => {
   return (
     <>
       <nav className="header-nav">
-        <img className="header-icon" src="src\assets\react-emblem.png"></img>
+        <img className="header-icon" src={emblem}></img>
         <h1 className="header-title">Task Organizer</h1>
         <button onClick={onClick} className="addTask">
           Add a Task
